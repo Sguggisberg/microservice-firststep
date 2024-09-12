7cfd9088dca9c1c0fe09290c8f9529476fb51cce
u240749 <stefan.guggisberg2@sbb.ch>
2024-09-12 13:59:02 +0200

    setup

---
+++ department/src/main/java/spring/microservice/first/step/department/DepartmentApplication.java
@@ -0,0 +1,13 @@
+package spring.microservice.first.step.department;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentApplication.class, args);
+	}
+
+}
