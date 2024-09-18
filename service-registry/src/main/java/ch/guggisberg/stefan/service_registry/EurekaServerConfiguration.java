d513d7aba5f9b9f4f7133f82ee693de7be7adf62
Stefan Guggisberg <stefanguggisberg76@gmail.com>
2024-09-18 09:11:53 +0200

    Eureka Project added

---
+++ service-registry/src/main/java/ch/guggisberg/stefan/service_registry/EurekaServerConfiguration.java
@@ -0,0 +1,9 @@
+package ch.guggisberg.stefan.service_registry;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@EnableEurekaServer
+@Configuration
+public class EurekaServerConfiguration {
+}
