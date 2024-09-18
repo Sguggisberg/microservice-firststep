aa9cd5fe7b43e410134789726f88ab87af8b7a4d
Stefan Guggisberg <stefanguggisberg76@gmail.com>
2024-09-18 10:00:03 +0200

    Eureka Client added

---
+++ department/src/main/java/spring/microservice/first/step/department/configuration/EurekaConfiguration.java
@@ -0,0 +1,9 @@
+package spring.microservice.first.step.department.configuration;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class EurekaConfiguration {
+}
---
+++ employee/src/main/java/spring/microservice/first/step/employee/configuration/EurekaConfiguration.java
@@ -0,0 +1,9 @@
+package spring.microservice.first.step.employee.configuration;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class EurekaConfiguration {
+}
